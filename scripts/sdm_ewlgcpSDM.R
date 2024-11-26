241eef152c3112cd1c9d4ab24a3d7ef1ff752352
frousseu <francois.rousseu@usherbrooke.ca>
2024-11-26 20:47:20 +0000

    inla mesh creation is now done a single time in the predictor script

--- scripts/sdm_ewlgcpSDM.R
+++ scripts/sdm_ewlgcpSDM.R
@@ -6,72 +6,20 @@ checkpoint("Running:")
 
 library(ewlgcpSDM)
 
-
-
-### Build mesh
-domain<-st_sample(st_buffer(region,5000),5000)
-domain <- inla.nonconvex.hull(st_coordinates(domain),convex = -0.015,resolution=75)
-
-
-pedge<-0.005
-edge<-min(c(diff(st_bbox(region)[c(1,3)])*pedge,diff(st_bbox(region)[c(2,4)])*pedge))
-edge
-
-mesh <- inla.mesh.2d(loc.domain = NULL,
-                     max.edge = c(edge,edge*3),
-                     min.angle = 21,
-                     cutoff = edge/1,
-                     offset = c(edge,edge*3),
-                     boundary = domain,#inla.mesh.segment(domainloc),
-                     crs = st_crs(region))
-
-
-#plan(multisession,workers=10)
-dmesh<-dmesh_mesh(mesh)
-#plan(sequential)
-
-### Compute weights
-dmesh<-dmesh_weights(dmesh,region)
-
-### Summarize predictors
-dmesh<-dmesh_predictors(dmesh,unwrap(predictors)[[vars]], progress = FALSE)
-
-
-#dm<-params$dmesh
-#dm$deciduous<-params$predictors$deciduous
-#plot(dm[,"deciduous"],border=NA,nbreaks=100,pal=function(x){rev(terrain.colors(x))},reset=FALSE,key.pos=NULL)
-#plot(st_geometry(qc),border=adjustcolor("black",0.25),add=TRUE)
-#plot(st_geometry(lakes),col="white",border=adjustcolor("black",0.25),add=TRUE)
-
-
 ### Summarize observations and effort
-
-buff<-st_buffer(obs,500000) |> st_union()
-#plotQC()
-#plot(st_geometry(buff),add=TRUE)
-#plot(st_geometry(obs),add=TRUE)
-
-
-dmesh<-dmesh_effort(dmesh,obs=d[d$presence==1,],background=d[d$presence%in%c(0,1),],buffer=buff,adjust=FALSE)
-
-#form<-paste("y ~",paste(c("tmax","tmax2","deciduous_esa","deciduous_esa2","conifers_esa","conifers_esa2"),collapse=" + ")) |> as.formula()
+buff <- st_buffer(obs, 500000) |> st_union()
+dmesh <- dmesh_effort(dmesh, obs = d[d$presence == 1, ], background =d[d$presence %in% c(0, 1), ], buffer = buff, adjust = FALSE)
 
 if(params$usepredictors=="Predictors"){
-  form<-paste("y ~",paste(c(vars,"tmax2"),collapse=" + ")) |> as.formula()
+  subvars <- c("conifers", "taiga", "deciduous", 
+"temperate_grassland", "wetland", "cropland", "urban", "water", "distfsl", "tmean", "geomflat", "elevation", "sand")
+  form<-paste("y ~",paste(c(subvars,"tmean2"),collapse=" + ")) |> as.formula()
 }else{
   form<-paste("y ~","dummy") |> as.formula()
 }
 
 
 ## Run model
-
-#dmesh$predictors[,1]<-runif(nrow(dmesh$predictors))
-
-#x<-dmesh$dmesh
-
-#x<-st_transform(x, paste0("+units=km +init=epsg:",epsg))
-#x<-st_transform(x,st_crs(x)$input)
-
 m<-ewlgcp(
   formula=form,
   dmesh=dmesh,
@@ -81,7 +29,7 @@ m<-ewlgcp(
   orthogonal = TRUE,
   prior.beta = NULL,#prior.beta<-list(prec=list(default=1/(0.000000001)^2,Intercept=1/(20)^2),mean=list(default=0,Intercept=0)),
   prior.range = c(5000,0.01),
-  prior.sigma = if(params$spatial=="Spatial"){c(1,0.01)}else{c(0.00001,NA)},
+  prior.sigma = if(params$spatial == "Spatial"){c(1, 0.01)}else{c(0.00001, NA)},
   smooth = 2,
   num.threads=1:1,
   #blas.num.threads=2,
--- scripts/sdm_predictors.R
+++ scripts/sdm_predictors.R
@@ -3,28 +3,66 @@ cat(paste("Running: predictors","/",Sys.time(),"\n"))
 
 library(terra)
 library(sf)
+library(ewlgcpSDM)
 
-predictors<-rast("data/predictors.tif")
-predictors<-predictors[[vars_pool]]
-predictors<-crop(predictors,vect(st_transform(region,st_crs(predictors))),mask=TRUE,touches=FALSE)
-#vars<-c(c("tmax","prec","trange","elevation","truggedness"),grep("_esa",names(predictors),value=TRUE))
-predictors<-project(predictors,crs(region),threads=4)
-predictors<-aggregate(predictors,4,na.rm=TRUE)
+predictors <- rast("data/predictors_300.tif")
+predictors <- predictors[[vars_pool]]
+predictors <- crop(predictors, vect(st_transform(region, st_crs(predictors))), mask = TRUE, touches = FALSE)
+if(crs(predictors) != crs(region)){
+    predictors <- project(predictors, crs(region), threads = 4)
+}
+predictors <- aggregate(predictors, 4, na.rm = TRUE)
 
-x<-init(predictors[[1]],"x")
-names(x)<-"x"
-y<-init(predictors[[1]],"y")
-names(y)<-"y"
-xy<-x*y
-names(xy)<-"xy"
-dummy<-init(predictors[[1]],fun=1)
-names(dummy)<-"dummy"
+x <- init(predictors[[1]], "x")
+names(x) <- "x"
+y <- init(predictors[[1]], "y")
+names(y) <- "y"
+xy <- x * y
+names(xy) <- "xy"
+dummy <- init(predictors[[1]], fun = 1)
+names(dummy) <- "dummy"
 
 
-predictors<-c(predictors,x,y,xy,dummy)
+predictors <- c(predictors, xy, dummy)
 
-predictors<-wrap(predictors)
+predictors <- wrap(predictors)
 
+
+###########################################
+### Build mesh for ewlgcpSDM ##############
+
+if(any(results$algorithm == "ewlgcpSDM")){
+
+    cat(paste("Running: dmesh","/",Sys.time(),"\n"))
+
+    ### Build mesh
+    domain <- st_sample(st_buffer(region, 5000), 5000)
+    domain <- inla.nonconvex.hull(st_coordinates(domain), convex = -0.015, resolution = 75)
+
+    edge <- min(c(diff(st_bbox(region)[c(1, 3)]) * dmesh_resolution, diff(st_bbox(region)[c(2, 4)]) * dmesh_resolution))
+    edge
+
+    mesh <- inla.mesh.2d(loc.domain = NULL,
+                        max.edge = c(edge, edge * 3),
+                        min.angle = 21,
+                        cutoff = edge / 1,
+                        offset = c(edge, edge * 3),
+                        boundary = domain,#inla.mesh.segment(domainloc),
+                        crs = st_crs(region))
+
+    plan(multisession, workers = 15)
+    dmesh <- dmesh_mesh(mesh)
+    plan(sequential)
+
+    ### Compute weights
+    dmesh <- dmesh_weights(dmesh, region)
+
+    ### Summarize predictors
+    plan(multisession, workers = 6)
+    dmesh <- dmesh_predictors(dmesh, predictors, progress = FALSE)
+    plan(sequential)
+
+}
 
 
 
